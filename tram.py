2f83082fcd149bbb8500f815b1629406e9d1295f
Verdejo <verdejod97@gmail.com>
2017-10-18 21:46:10 +0200

    Update tram.py
    Intervalos añadidos

--- tram.py
+++ tram.py
@@ -3,6 +3,8 @@
 #   `--> (TramBot SA no se responsabiliza de gente que no sepa leer, gracias)
 #
 #    Desing By TitoVerdejus and SecuNabo
+#
+# **Escribir tu TOKEN, debes tener el archivo paradas.json en el mismo directorio que este bot para que funcione
 ###########################################################################################################
 #⌚️
 #📌
@@ -17,7 +19,7 @@ def start(bot,update):
     #username de quien manda el mensaje para saludar
     name=update.message.from_user.name
     
-    update.message.reply_text('Hola '+ name +'soy TramBot escribe /help para obtener ayuda.')
+    update.message.reply_text('Hola '+ name +' soy TramBot escribe /help para obtener ayuda.')
 
 
 #
@@ -26,7 +28,7 @@ def start(bot,update):
 #  Mostrar ayuda detallada de cada comando
 #
 def help(bot,update):
-    update.message.reply_text('Ayuda:\n/start - Saludo del bot.\n/paradas - Paradas disponibles.\n/times Origen Destino - Horarios de hoy.\n/timesto Origen Destino HInicio HFin - Horarios en un intervalo de tiempo.\n/help - Muestra la ayuda.')
+    update.message.reply_text('Ayuda:\n/start - Saludo del bot.\n/paradas - Paradas disponibles.\n/times Origen Destino - Horarios de hoy.\n/timesto Origen Destino HInicio HFin - Horarios en un intervalo de tiempo, formato de hora XX:XX.\n/help - Muestra la ayuda.')
 
 
 
@@ -46,7 +48,6 @@ def timestoday(bot,update,args):
         today = time.strftime("%x")
 
         #Cargo el json de las paradas y sus respectivos codigos
-        #CAMBIAR UBICACION DEL JSON*************************************************
         leer = json.loads(open('./paradas.json').read())
 
         #Valida si existe el argumento 1 (origen) y el argumento 2 (destino)
@@ -80,9 +81,9 @@ def timestoday(bot,update,args):
                 cadatd = re.findall("<td>(\d+:\d+)</td>", r.text)
 
                 total=''
-                numero1='0'
-                numero0='0'
-                #Mando el horario de la froma, en una linea la hora y en la siguiente todas las
+                numero1='-1'
+                numero0='-1'
+                #Mando el horario de la forma: en una linea la hora y en la siguiente todas las
                 #veces que sale en esa hora, y asi con todas las horas.
                 for hora in cadatd:
                     #imprime horas por grupos de hora
@@ -91,11 +92,15 @@ def timestoday(bot,update,args):
                         total=(total+hora+' ')
                     #detecta cuando tiene que pasar a la siguiente hora
                     else:
+                        #Detectar transbordo
+                        if(numero0*10+numero1 > hora[0]*10+hora[1]):
+                            total=total+'\n\nTRANSBORDOOOR\n'
+                        
                         numero0=hora[0]
                         numero1=hora[1]
                         total=(total+'\n'+'⌚️'+numero0+numero1+'\n'+'📌'+hora+' ')
 
-                update.message.reply_text('Salidas desde **'+args[0]+'** a **'+args[1]+ '** :\n'+total)
+                update.message.reply_text('Salidas desde '+args[0]+' a '+args[1]+ ' :'+total)
 
     else:
         update.message.reply_text("Error en los parametros.\nUso /times Origen Destino")
@@ -103,57 +108,82 @@ def timestoday(bot,update,args):
 # Funcion TimesIntervalo: devuelve el horario de salidas de una estacion origen a una destino en un intervalo de horas
 # establecido por el usuario (como argumento 2 y 3)
 #
-# [*] USO: /timesto Bulevar del Pla Alicante-Luceros 7 9
+# [*] USO: /timesto Bulevar-del-Pla-Alicante-Luceros 7 9
 #
-#  [+] Implementar: obtener la salida y destino (en plan): "times SantaPolla AlacantPuteros"
-#     obtener ese destino y salida, obtener numeros de la estacion y añadirlos a la queryhttp
 #
 def timesintervalo(bot,update,args):
 
     if(len(args) == 4):
 
-        print("Leer archivos")
         #Cambiar ubicacion del JSON **************************************************
         leer = json.loads(open('./paradas.json').read())
         
         #Valida si existe el argumento 1 (origen) y el argumento 2 (destino)
         numid = leer.get(args[0], 'Nothing')
         numid2 = leer.get(args[1], 'Nothing')
-        
-        cadatd = re.findall("(\d+:\d+)", args[2])
-        update.message.reply_text('Pillado '+ args[0])
-
 
         #Compruebo que el origen y el destino sean distintos
         if(args[0]==args[1]):
             update.message.reply_text('Error.\nEl origen y destino coinciden!')
         
         else:
-
-            if((args[2].isdigit()) and (args[3].isdigit())):
-                print('Arg3  y Arg4 son numeros')
+            #Comprobacion del formato de hora correcto
+            if(len(args[2])==5 and len(args[3])==5 and args[2][2]==':'and args[3][2]==':'and args[2][0].isdigit() and args[2][1].isdigit() and args[2][3].isdigit() and args[2][4].isdigit() and args[3][0].isdigit() and args[3][1].isdigit()and args[3][3].isdigit() and args[3][4].isdigit()):
 	    	
-                #Obtengo la fecha actual
-                today = time.strftime("%x")
+                #Valida si ha encontrado la codificacion de la parada de origen y destino
+                if((numid in "Nothing") or (numid2 in "Nothing")):
+                    update.message.reply_text("Error en las paradas.\nEs posible que haya errores en los nombres de las paradas")
+                else:
+			
+                    today = time.strftime("%x")
+                
+                    #Hora inicio y final
+                    hinicio = args[2]
+                    hfinal = args[3]
 
-                #Construyo y realizo la peticion con los datos correspondientes
-                payload = {
-			'	origen': '124',
+
+                
+                    #Construyo y realizo la peticion con los datos correspondientes y los intervalos
+                    payload = {
+				'origen': numid,
 				'aceptar': '0',
 				'key': '0',
-				'destino': '2',
+				'destino': numid2,
 				'fecha': today,
-				'hini': '00:00',
-				'hfin': '23:59',
+				'hini': hinicio,
+				'hfin': hfinal,
 				'calcular': '1'
-                }
-                r = requests.post("http://www.tramalicante.es/horarios.php", data=payload)
+                    }
+                    r = requests.post("http://www.tramalicante.es/horarios.php", data=payload)
 
-                #Scrapeo cada valor de la tabla de horarios
-                cadatd = re.findall("<td>(\d+:\d+)</td>", r.text)
-                print('Args: %s' % args[0:4])
+                    #Scrapeo cada valor de la tabla de horarios
+                    cadatd = re.findall("<td>(\d+:\d+)</td>", r.text)
+
+                    total=''
+                    numero1='-1'
+                    numero0='-1'
+                    #Mando el horario de la forma: en una linea la hora y en la siguiente todas las
+                    #veces que sale en esa hora, y asi con todas las horas.
+                
+                    for hora in cadatd:
+                        #imprime horas por grupos de hora
+                        if(hora[0]==numero0 and hora[1]==numero1):
+                       
+                            total=(total+hora+' ')
+                        #detecta cuando tiene que pasar a la siguiente hora
+                        else:
+                            #Detectar transbordo
+                            if(numero0*10+numero1 > hora[0]*10+hora[1]):
+                                total=total+'\n\nTRANSBORDOOOR\n'
+                            
+                            numero0=hora[0]
+                            numero1=hora[1]
+                            total=(total+'\n'+'⌚️'+numero0+numero1+'\n'+'📌'+hora+' ')
+
+                    update.message.reply_text('Salidas desde '+args[0]+' a '+args[1]+ 'de '+hinicio+ ' a '+hfinal+ ':'+total)
+           
             else:
-                update.message.reply_text('El intervalo de horas es incorrecto, deben ser valores numéricos!')
+                update.message.reply_text('El intervalo de horas es incorrecto, debe cumplir este formato: XX:XX')
     else:
         update.message.reply_text('Error en los argumentos.\nUso: /timesto Origen Destino Hora1 Hora2')
 
@@ -170,12 +200,13 @@ def paradas(bot, update):
     total='📌'
     for parada in leer:
 
-        total=(total+parada+'📌')
-    
+        total=(total+parada+'\n'+'📌')
+    temp=len(total)
+    total=total[:temp -1] 
     update.message.reply_text('Paradas disponibles:\n'+total)
 
 ######################################
-# Funcion principal 		             #
+# Funcion principal 		     #
 ######################################
 def main():
     #Cambiar token***********************************************************************
